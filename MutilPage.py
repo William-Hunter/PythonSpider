a515a4cc914e611ddd5bc7f52f5145a809184264
William-Hunter <a709209619@qq.com>
2016-05-11 22:56:31 +0800

    完成了对文件的输出

--- MutilPage.py
+++ MutilPage.py
@@ -5,30 +5,31 @@ import sys
 from BeautifulSoup import BeautifulSoup
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
-
+os.system("cd target")
+os.system("rm -rf *.txt")
 ranger={'0~200','200~400','400~600','600~800','800~1000','1000~1200','1200~1400','1400~1600','1600~1800','1800~2000','2000~2200','2200~2400','2400~2600','2600~2800','2800~3000','3000~3100'}
-os.remove("target/*.txt")
-# for ran in ranger:
-# 	fp=open("target/"+ran+".txt","w")
-# 	url="http://bbs.dvd9000.com/html/"+ran+".html"
-# 	fp.write(url+"\n\n")
-# 	doc = urllib2.urlopen(url)
-# 	print "\n-------------",url
-# 	html = BeautifulSoup(doc)
-# 	tdlist=html.findAll('td',align='left')
-# 	i=1
-# 	for td in tdlist:
-# 		print i
-# 		i+=1
-# 		try:
-# 			subtitle=td.contents[2]
-# 		except TypeError, e:
-# 			subtitle=''
-# 		try:
-# 			link=td.a['href']	
-# 		except TypeError, e:
-# 			link=''
-# 		fp.write(subtitle+"\n")
-# 		fp.write(link+"\n\n")
-# 	fp.close()
+for ran in ranger:
+	fp=open("target/"+ran+".txt","w")
+	url="http://bbs.dvd9000.com/html/"+ran+".html"
+	fp.write(url+"\n\n")
+	doc = urllib2.urlopen(url)
+	print "\n-------------",url
+	html = BeautifulSoup(doc)
+	tdlist=html.findAll('td',align='left')
+	i=1
+	for td in tdlist:
+		print i
+		i+=1
+		try:
+			subtitle=str(td.contents[2])
+			print subtitle
+		except TypeError, e:
+			subtitle=''
+		try:
+			link=td.a['href']	
+		except TypeError, e:
+			link=''
+		fp.write(subtitle+"\n")
+		fp.write(link+"\n\n")
+	fp.close()
 			
