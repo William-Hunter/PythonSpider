40c20edbe0d3ae661640fae85894ec94aeaab5d5
William-Hunter <a709209619@qq.com>
2016-05-13 08:04:38 +0800

    测试邮箱

--- Email.py
+++ Email.py
@@ -5,24 +5,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText  
 from email.mime.image import MIMEImage  
   
-sender = '***'  
-receiver = '***'  
+username = 'willliam113758@126.com'  
+password = 'lonedon'
+sender = username  
+receiver = 'a709209619@qq.com'  
 subject = 'python email test'  
-smtpserver = 'smtp.163.com'  
-username = '***'  
-password = '***'  
+servername = 'smtp.exmail.126.com'  
   
+  	
 msgRoot = MIMEMultipart('related')  
 msgRoot['Subject'] = 'test message'  
   
 #构造附件  
-att = MIMEText(open('h:\\python\\1.jpg', 'rb').read(), 'base64', 'utf-8')  
+att = MIMEText(open('target/0~200.txt', 'rb').read(), 'base64', 'utf-8')  
 att["Content-Type"] = 'application/octet-stream'  
-att["Content-Disposition"] = 'attachment; filename="1.jpg"'  
+att["Content-Disposition"] = 'attachment; filename="0~200.txt"'  
 msgRoot.attach(att)  
-          
-smtp = smtplib.SMTP()  
-smtp.connect('smtp.163.com')  
-smtp.login(username, password)  
-smtp.sendmail(sender, receiver, msgRoot.as_string())  
+
+smtp = smtplib.SMTP()
+print 1
+smtp.connect(servername)  
+print 2
+print "login"+smtp.login(username, password)  
+print "send"+smtp.sendmail(sender, receiver, msgRoot.as_string())  
 smtp.quit() 
