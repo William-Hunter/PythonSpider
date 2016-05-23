6712f2b3914bbf167b39cd59788ccd10a733f6d2
William-Hunter <a709209619@qq.com>
2016-05-23 22:57:05 +0800

    针对有目录的文章结构进行抓取

--- Contents.py
+++ Contents.py
@@ -1,14 +1,18 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
-
-print "start"
-
-doc = urllib2.urlopen("http://bbs.dvd9000.com/html/0~200.html")
+homepage='http://tt.71wx.net/xiaoshuo/5/5704/'
+doc = urllib2.urlopen(homepage)
 html = BeautifulSoup(doc)
+menu=html.findAll('div','centent')
+contents=menu[0].findAll('a')
+for e in contents:
+	print '------'
+	print e.string
+	# fp=open("airticle/"+e.string+".txt","w")	
+	print homepage+e['href']
+	chapters=urllib2.urlopen(homepage+e['href'])
+	chapters = BeautifulSoup(chapters)
+	print chapters
 
-menu=html.findAll('div',id='postnavi')
-print menu
-# BeautifulSoup(menu)
-# print menu.findAll("a")
 
 
--- Email.py
+++ Email.py
@@ -5,14 +5,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText  
 from email.mime.image import MIMEImage
 servername = 'smtp.126.com'    
-username = 'willliam113758@126.com'  
-password=raw_input("input password of willliam113758@126.com:")
-receiver = raw_input("input receiver:") 
+# username = raw_input("input username:")
+username = "iwantfunkyou@126.com"
+# password=raw_input("input password:")
+password="fuckfuckfuck"
+# receiver = raw_input("input receiver:")
+receiver = "1344014631@qq.com"
 msgRoot = MIMEMultipart('related')  
-Subject=raw_input("input the email subject:")
-msgRoot['Subject'] = Subject 
+msgRoot['Subject'] = "shit"
 #构造附件 
-att = MIMEText(open(raw_input("input file directer:"), 'rb').read(), 'base64', 'utf-8')  
+att = MIMEText(open("target/0~200.txt", 'rb').read(), 'base64', 'utf-8')  
 att["Content-Type"] = 'application/octet-stream'  
 att["Content-Disposition"] = 'attachment; filename="0~200.txt"'  
 msgRoot.attach(att)  
