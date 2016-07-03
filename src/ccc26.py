36a21332eb343d0072e10c09ab3b43f9a2687d9b
William-Hunter <a709209619@qq.com>
2016-07-03 23:04:13 +0800

    完成了python的下载图片功能

---
+++ src/ccc26.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+#coding=utf-8
+import urllib2
+import traceback
+import os
+import sys
+import requests
+from BeautifulSoup import BeautifulSoup
+
+homepage='http://www.u6f4.com'
+folder = "../pornimg/"
+
+def menu():	
+	request = urllib2.Request("http://www.u6f4.com/AAtupian/AAtb/")
+	request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36')
+	doc = urllib2.urlopen(request)
+	html = BeautifulSoup(doc)
+	category=html.findAll("li",{"class":"menu-item"})
+	index=0
+	menu=[]
+	for zoo in category:
+		menu.append(homepage+zoo.a['href'])
+		print index,":",zoo.a.text
+		index=index+1
+
+	page(menu[input("input which zone is you like:")])
+	
+def page(pagePath):
+	request = urllib2.Request(pagePath)
+	request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36')
+	doc = urllib2.urlopen(request)
+	html = BeautifulSoup(doc)
+	viewlist=html.findAll("div",{"class":"box list channel"})
+	viewlinks=viewlist[0].findAll("a")
+	for viewlink in viewlinks:
+		print viewlink.text,"page"
+		view(homepage+viewlink['href'],viewlink.text)
+
+def view(viewpath,title):
+	if not os.path.exists(folder+title):
+		os.makedirs(folder+title)
+
+	request = urllib2.Request(viewpath)
+	request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36')
+	doc = urllib2.urlopen(request)
+	html = BeautifulSoup(doc)
+	viewlist=html.findAll("img")
+	for img in viewlist:
+		download(img['src'],title)
+
+def download(src,title):
+	srctrim=src.split("/")
+	srctrim=srctrim[len(srctrim)-1]
+
+	imgname=srctrim+".jpg"
+	path = folder+title+"/"+imgname
+	request = urllib2.Request(src)
+	request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36')
+	conn = urllib2.urlopen(request)
+	f = open(path,'w')  
+	f.write(conn.read())  
+	f.close()  
+	print(imgname+'\t\tSaved!')
+
+
+if __name__ == '__main__':
+	reload(sys)
+	print "START"
+
+	# view("http://www.u6f4.com/AAtupian/AAwz/e225845aff406b21ce8ab3a0860be756.html","yaotou")
+	
+	function={ '1':menu	}
+	function.get(raw_input("1:ccc26Porn image\ninput:"))()
+
+	print "END"
--- src/github.py
+++ src/github.py
@@ -1,19 +1,31 @@
+#!/usr/bin/python
 #coding=utf-8
+
 import urllib2
 from BeautifulSoup import BeautifulSoup
-homepage='https://github.com'
-def work(homepage,key):
-	doc = urllib2.urlopen(homepage)
-	html = BeautifulSoup(doc)
-	filelistlink=html.findAll('a','js-navigation-open')
-	if(filelistlink):
-		print "TRUETRUETRUETRUETRUETRUETRUETRUETRUETRUETRUETRUETRUE"
-		for row in filelistlink:
-			print '-------------------------------------------------------------------------------------------------------------'
-			print row		
-	else:
-		print "FALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSE"
+
+# def work(page,key):
+# 	doc = urllib2.urlopen(page)
+# 	html = BeautifulSoup(doc)
+# 	filelistlink=html.findAll('a','js-navigation-open')
+# 	if(filelistlink):
+# 		print "TRUETRUETRUETRUETRUETRUETRUETRUETRUETRUETRUETRUETRUE"
+# 		for row in filelistlink:
+# 			print '-------------------------------------------------------------------------------------------------------------'
+# 			print row		
+# 	else:
+# 		print "FALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSE"
+
+
+
+
+def do(self):
+	print "hello"
+
 
 if __name__ == '__main__':
-	work('https://github.com/Titzanyic/RiddleAndroidProject','')
+	homepage='https://github.com'
+	do()
+# 	twork('https://github.com/Titzanyic/RiddleAndroidProject','')
 	print "END"
+
