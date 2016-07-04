d2319f1ec0d10344f024caf0348dcc38e5c853ac
William-Hunter <a709209619@qq.com>
2016-07-04 22:37:31 +0800

    完善了关键字搜索的功能

--- src/ccc26.py
+++ src/ccc26.py
@@ -9,6 +9,7 @@ from BeautifulSoup import BeautifulSoup
 
 homepage='http://www.u6f4.com'
 folder = "../pornimg/"
+keyword='aaaa'
 
 def menu():	
 	request = urllib2.Request("http://www.u6f4.com/AAtupian/AAtb/")
@@ -22,19 +23,29 @@ def menu():
 		menu.append(homepage+zoo.a['href'])
 		print index,":",zoo.a.text
 		index=index+1
-
-	page(menu[input("input which zone is you like:")])
+	menuindex=menu[input("请输入进入哪个菜单:")]
+	keyword=raw_input("输入你想要的关键字:")
+	page(menuindex,keyword)
 	
-def page(pagePath):
+def page(pagePath,keyword):
 	request = urllib2.Request(pagePath)
 	request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36')
 	doc = urllib2.urlopen(request)
 	html = BeautifulSoup(doc)
 	viewlist=html.findAll("div",{"class":"box list channel"})
+	
 	viewlinks=viewlist[0].findAll("a")
 	for viewlink in viewlinks:
-		print viewlink.text,"page"
-		view(homepage+viewlink['href'],viewlink.text)
+		if keyword in viewlink.text:
+			print viewlink.text,"page"
+			view(homepage+viewlink['href'],viewlink.text)
+
+	nextpagelist = html.findAll("a", {"class" : "pagegbk"})
+	for next in nextpagelist:
+		if "下一页" in next.text:
+			nextpage=homepage+next['href']
+			print 'nextpage-------',nextpage
+			page(nextpage,keyword)
 
 def view(viewpath,title):
 	if not os.path.exists(folder+title):
@@ -65,6 +76,7 @@ def download(src,title):
 
 if __name__ == '__main__':
 	reload(sys)
+	sys.setdefaultencoding('utf-8') 
 	print "START"
 
 	# view("http://www.u6f4.com/AAtupian/AAwz/e225845aff406b21ce8ab3a0860be756.html","yaotou")
